e3d2a3b9e7d7265e13c9d3d38fcb4cb21cdba471
BumbleFlash <sudarshan97@gmail.com>
2021-04-17 09:51:56 -0400

    fix: fixed employee add API bug

--- models/employee.model.js
+++ models/employee.model.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 
 const Employee = mongoose.model("Employee",
     new mongoose.Schema({
-            name: String,
-            email: String,
-            base_salary: Number,
-            bonus: Number,
-            insurance: Number,
-            four_o_oneK: Number,
-            totalSalary: Number,
-            recruitedBy: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "User"
-            },
+        name: String,
+        email: String,
+        base_salary: Number,
+        bonus: Number,
+        insurance: Number,
+        four_o_oneK: Number,
+        total_salary: Number,
+        recruitedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        updatedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
     })
 );
 
